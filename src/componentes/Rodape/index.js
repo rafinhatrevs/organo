9f98ed921bb5fbbfbcdc137c908e0ce724e3effd
Rafaella Trevizan <rafaella_trevizan@hotmail.com>
2024-12-13 19:43:11 -0300

    subindo projeto organo

---
+++ src/componentes/Rodape/index.js
@@ -0,0 +1,17 @@
+import './Rodape.css'
+
+const Rodape = () => {
+    return (
+        <footer className="rodape">
+            <div className="redes">
+                <img src='/imagens/fb.png' alt='' />
+                <img src='/imagens/tw.png' alt='' />
+                <img src='/imagens/ig.png' alt='' />
+            </div>
+            <img src='/imagens/logo.png' alt='' />
+            <p>Desenvolvido por Alura.</p>
+        </footer>
+    )
+}
+
+export default Rodape
